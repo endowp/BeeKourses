f65621a4fd29276a6500a692e69b3a4c71e26b22
Endowp <endo_899@hotmail.com>
2021-02-02 23:55:09 +0700

    add more sites

--- web.js
+++ web.js
@@ -1,3 +1,14 @@
+/*const la1_name = ["aKhan Academy", "aCoursera", "LinkedIn x Microsoft", "Future Learn", "aTyping.com"]; 
+
+window.onload = function() {
+    const la1_query = document.getElementsByTagName("");
+    let i = 0;
+    for(var j = 0; j < la1_query.length; j++) {
+        la1_query[j].innerHTML = la1_name[i];
+        i++;
+    }
+};*/
+
 function inProgress(){
     alert("This section is in progress :D");
 }
